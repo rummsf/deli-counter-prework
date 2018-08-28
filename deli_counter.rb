ef1e1c8a840b6f03fbf023c20427a505062e796d
Irum F <irummf@gmail.com>
2018-08-28 21:45:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,6 @@ def nowServing (katzDeliLine)
   if katzDeliLine.length > 0
 return "Currently serving " + katzDeliLine.shift() + "." 
   else "There is nobody waiting to be served!"
-end 
 
 def currentLine(line) {
   if !line.length
